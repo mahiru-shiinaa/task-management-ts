3720f95bb1387a20ab8b224c0a48d2642132ca61
Mahiru Shiina <halovemusics@gmail.com>
2025-07-10 13:20:00 +0700

    Cấu hình CORS

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import * as database from "@config/database";
 import mainV1Routes from "@routes/index.route";
-
+import cors from "cors";
 
 
 
@@ -16,6 +16,8 @@ const port: number | string = process.env.PORT || 3000;
 // Middleware để đọc body từ client, không cần body-parser nâng cao
 app.use(express.json()); // Đọc JSON từ client (axios/fetch gửi lên)
 app.use(express.urlencoded({ extended: true })); // Nếu dùng form HTML gửi lên
+
+app.use(cors());
 
 mainV1Routes(app);
 
